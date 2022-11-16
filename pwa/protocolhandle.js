5c0f9e30efd85d4420f69db1aff579239348d0e9
Nailington <111466491+Nailington@users.noreply.github.com>
2022-11-16 10:35:16 -0500

    More Protocol changes

--- pwa/protocolhandle.js
+++ pwa/protocolhandle.js
@@ -4,8 +4,8 @@
 // navigator.registerProtocolHandler("web+sega", "launchpad.html?gameID=%s&gametitle=%s&fav=%s", "Sega handler"); // last title arg included for compatibility
 
 document.querySelector('button').addEventListener('click', () => {
-    navigator.registerProtocolHandler("web+gba", "launchpad.html?gameID=%s&gametitle=%s&fav=%s", "GBA handler"); // last title arg included for compatibility
-navigator.registerProtocolHandler("web+nes", "nes.html", "NES handler"); // last title arg included for compatibility
-navigator.registerProtocolHandler("web+snes", "snes.html", "SNES handler"); // last title arg included for compatibility
-navigator.registerProtocolHandler("web+sega", "segaMD.html", "Sega handler"); // last title arg included for compatibility;
+    navigator.registerProtocolHandler("web+gba", "%s", "GBA handler"); // last title arg included for compatibility
+navigator.registerProtocolHandler("web+nes", "%s", "NES handler"); // last title arg included for compatibility
+navigator.registerProtocolHandler("web+snes", "%s", "SNES handler"); // last title arg included for compatibility
+navigator.registerProtocolHandler("web+sega", "%s", "Sega handler"); // last title arg included for compatibility
   });
