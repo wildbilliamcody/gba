21367d3e9a071b40e74e10f1770984c5080cc032
Logan <thecrazykid2121@gmail.com>
2022-12-14 21:41:04 -0500

    push to public beta
    Co-Authored-By: Nailington <111466491+Nailington@users.noreply.github.com>

--- data/js/swap.js
+++ data/js/swap.js
@@ -3,6 +3,13 @@ let is1Check = 0;
 let isNet = 0;
 let doOldCores = false;
 
+
+if (gameCoreType == "nds") {
+    isCheck = 1;
+    localStorage.setItem("isCheck", JSON.stringify(isCheck));
+    console.log("nds")
+}
+
 let isNetDat = localStorage.getItem("isNet");
 isNet = JSON.parse(isNetDat);
 
@@ -31,7 +38,7 @@ if (isNet == 0) {
     }
     if (userCustom == true) {
         gameName = gameText
-        let gameLink = customHost + gameName + ".zip";
+        let gameLink = custHost + gameName + ".zip";
         gameName = gameLink;
         console.log("custom-server")
     }
@@ -54,6 +61,8 @@ if (isNet == 0) {
     let gameLink = "https://rawcdn.githack.com/mathadventure1/nds-host/a15becf40ff31c1f8fd57b8ca65254559311bbad/nds-alt/" + gameName + ".zip";
     gameName = gameLink;
     console.log("nds-server");
+    isCheck = 0;
+    localStorage.setItem("isCheck", JSON.stringify(isCheck));
     }
     let ischeckDatas = localStorage.getItem("is1Check");
     is1Check = JSON.parse(ischeckDatas);
